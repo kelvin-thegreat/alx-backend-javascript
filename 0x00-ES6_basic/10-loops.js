8569add8658de6c60ccd35e3c7bc8733e96d4eb0
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-25 17:37:20 +0300

    using ES6 method properties

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const appendArray = [];  
   for (const value of array) {
     appendArray.push(appendString + value);
-    // value = appendString + value;
+    
   }
 
   return appendArray;
