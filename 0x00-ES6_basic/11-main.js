8ec406a1dfcadf1eef8c890f43f251caba773219
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-26 19:55:33 +0300

    Moved test scripts to folder test

--- 0x00-ES6_basic/11-main.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/extensions
-import createEmployeesObject from './11-createEmployeesObject.js';
-
-console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
