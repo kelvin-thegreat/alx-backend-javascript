48747bb713099220003d2f6038dec9bb7ba690ef
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-26 19:29:00 +0300

    create a report object

--- 0x00-ES6_basic/11-main.js
+++ 0x00-ES6_basic/11-main.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/extensions
 import createEmployeesObject from './11-createEmployeesObject.js';
 
-console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
+console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
