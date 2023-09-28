2db419787d4d6f9d5becf408953808b6420ecf39
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-28 23:21:44 +0300

    task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,10 @@
+/*
+* Define a ClassRoom class
+*/
 export default class ClassRoom {
+  /* Constructor to initialize the class */
   constructor(maxStudentsSize) {
+    /* Store the maximum student size as an instance variable */
     this._maxStudentsSize = maxStudentsSize;
   }
 }
