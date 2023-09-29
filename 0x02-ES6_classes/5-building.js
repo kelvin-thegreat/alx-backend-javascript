9366f6069eec92dd5b0f09871b3495d60cbf52ea
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 16:57:50 +0300

    Task 4

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,46 +2,21 @@
  * Class representing a building with square footage.
  */
 export default class Building {
-    /**
-     * Create a new Building instance.
-     * @param {number} sqft - The square footage of the building.
-     */
     constructor(sqft) {
       this.sqft = sqft;
-  
-      // Check if this constructor is invoked directly.
       if (this.constructor !== Building) {
-        // Ensure that a subclass overrides the evacuationWarningMessage method.
-        if (typeof this.evacuationWarningMessage !== 'function') {
+            if (typeof this.evacuationWarningMessage !== 'function') {
           throw new Error(
             'Class extending Building must override evacuationWarningMessage',
           );
         }
       }
     }
-  
-    /**
-     * Get the square footage of the building.
-     * @returns {number} - The square footage.
-     */
     get sqft() {
       return this._sqft;
     }
-  
-    /**
-     * Set the square footage of the building.
-     * @param {number} value - The square footage to set.
-     */
     set sqft(value) {
       this._sqft = value;
     }
-  
-    /**
-     * This method should be overridden by subclasses to provide an evacuation warning message.
-     * @throws {Error} - An error is thrown if not overridden.
-     */
-    evacuationWarningMessage() {
-      throw new Error('evacuationWarningMessage must be implemented by subclasses');
-    }
-  }
+ }
   
