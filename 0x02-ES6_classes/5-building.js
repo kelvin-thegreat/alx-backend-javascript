f8a88a4a003476418d4a814c6ed1c9c103cc4216
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 17:00:27 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,22 +1,25 @@
 /**
- * Class  building with square footage.
+ * Class representing a building with square footage.
  */
 export default class Building {
     constructor(sqft) {
-      this.sqft = sqft;
+      this._sqft = sqft;
+            
       if (this.constructor !== Building) {
-            if (typeof this.evacuationWarningMessage !== 'function') {
+        if (typeof this.evacuationWarningMessage !== 'function') {
           throw new Error(
             'Class extending Building must override evacuationWarningMessage',
           );
         }
       }
     }
+  
     get sqft() {
       return this._sqft;
     }
+  
     set sqft(value) {
       this._sqft = value;
     }
- }
+  }
   
