43da8b65e416848fc78f89da55960f92da40e350
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 14:24:18 +0300

    Task 5

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,29 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      {
+        status: userResult.status,
+        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
+      },
+      {
+        status: photoResult.status,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
+      },
+    ];
+  } catch (error) {
+    return [
+      {
+        status: 'rejected',
+        value: error.message,
+      },
+    ];
+  }
+}
