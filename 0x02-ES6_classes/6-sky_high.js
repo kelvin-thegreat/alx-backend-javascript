ac46bbdcb72ad171a39eea5c08e2a1daf434f953
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 17:13:36 +0300

    Task 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,45 +1,20 @@
-  /**
-   * Import the Building class from the 5-building module
-   * 
-   */
 import Building from './5-building';
 
-  /**
-   * Define a subclass SkyHighBuilding that extends the Building class
-   */
 export default class SkyHighBuilding extends Building {
-  /**
-   * Create a new SkyHighBuilding instance.
-   * @param {number} sqft - The square footage of the building.
-   * @param {number} floors - The number of floors in the building.
-   */
   constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
   }
 
-  /**
-   * Get the number of floors in the building.
-   * @returns {number} - The number of floors.
-   */
   get floors() {
-    return this._floors; // Use _floors to store the value
+    return this._floors;
   }
 
-  /**
-   * Set the number of floors in the building.
-   * @param {number} value - The number of floors to set.
-   */
   set floors(value) {
-    this._floors = value; // Use _floors to store the value
+    this._floors = value;
   }
 
-  /**
-   * Override the evacuationWarningMessage method to provide a custom message.
-   * @returns {string} - The evacuation warning message.
-   */
   evacuationWarningMessage() {
-    // Return a message with the number of floors
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }
