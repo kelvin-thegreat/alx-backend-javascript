6b26829edf33e11b1f6d27a932691e26b2b4bb75
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 17:16:57 +0300

    Task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,27 @@
-class Airport {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
+export default class Airport {
+    constructor(name, code) {
+      this.name = name;
+      this.code = code;
+    }
+  
+    set name(value) {
+      this._name = value;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set code(value) {
+      this._code = value;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this._code;
+    }
   }
   
-  toString() {
-    return `[${this._code}]`;
-  }
-}
-
-export default Airport;
