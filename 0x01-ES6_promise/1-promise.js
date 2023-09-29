f8e85586526b63432dee54b1f34134e32ab9c69e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 11:36:44 +0300

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,12 @@
+// 1-promise.js
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
+      // If 'success' is true, resolve the promise
+      const response = { status: 200, body: 'Success' };
+      resolve(response);
     } else {
+      // If 'success' is false, reject the promise with an error message
       reject(new Error('The fake API is not working currently'));
     }
   });
