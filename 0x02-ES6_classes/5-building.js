1a744891d10c6459b372de8893e0b01bd98b1531
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 16:55:41 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,34 +1,47 @@
 /**
- * Abstract class representing a building.
+ * Class representing a building with square footage.
  */
 export default class Building {
-  /**
-   * Create a new Building instance.
-   * @param {number} sqft - The square footage of the building.
-   */
-  constructor(sqft) {
     /**
-     * The square footage of the building.
-     * @type {number}
-     * @private
+     * Create a new Building instance.
+     * @param {number} sqft - The square footage of the building.
      */
-    this._sqft = sqft;
+    constructor(sqft) {
+      this.sqft = sqft;
+  
+      // Check if this constructor is invoked directly.
+      if (this.constructor !== Building) {
+        // Ensure that a subclass overrides the evacuationWarningMessage method.
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error(
+            'Class extending Building must override evacuationWarningMessage',
+          );
+        }
+      }
+    }
+  
+    /**
+     * Get the square footage of the building.
+     * @returns {number} - The square footage.
+     */
+    get sqft() {
+      return this._sqft;
+    }
+  
+    /**
+     * Set the square footage of the building.
+     * @param {number} value - The square footage to set.
+     */
+    set sqft(value) {
+      this._sqft = value;
+    }
+  
+    /**
+     * This method should be overridden by subclasses to provide an evacuation warning message.
+     * @throws {Error} - An error is thrown if not overridden.
+     */
+    evacuationWarningMessage() {
+      throw new Error('evacuationWarningMessage must be implemented by subclasses');
+    }
   }
-
-  /**
-   * Get the square footage of the building.
-   * @returns {number} The square footage.
-   */
-  get sqft() {
-    return this._sqft;
-  }
-
-  /**
-   * Abstract method for providing an evacuation warning message.
-   * Subclasses must implement this method.
-   * @abstract
-   */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
+  
