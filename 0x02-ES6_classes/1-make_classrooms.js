150b79c84fdb73b057358e8d8d6a71d6fa502f7e
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-09-29 16:14:55 +0300

    Task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
 import ClassRoom from './0-classroom';
 
-/**
- * Creates an array of ClassRooms with sizes.
- */
 export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
