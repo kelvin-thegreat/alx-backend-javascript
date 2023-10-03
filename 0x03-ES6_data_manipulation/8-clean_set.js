38cec26eaf9f3f5057e4628856b11a7d31f21fd6
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-03 11:54:58 +0300

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,22 @@
  * @param {string} startString - The string to check for as a prefix.
  * @returns {string} - A string containing filtered values separated by '-'.
  */
+/**
+ * Returns a string of set values that start with a specific string.
+ *
+ * @param {Set} set - The Set to filter values from.
+ * @param {string} startString - The string to check for as a prefix.
+ * @returns {string} - A string containing filtered values separated by '-'.
+ */
 export default function cleanSet(set, startString) {
-    const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
-    return filteredValues.join('-');
-  }  
+    return (
+      !set || !startString || !(set instanceof Set) || typeof startString !== 'string'
+        ? ''
+        : Array.from(set)
+            .filter(value => typeof value === 'string' && value.startsWith(startString))
+            .map(value => value.substring(startString.length))
+            .filter(value => value !== '')
+            .join('-')
+    );
+  }
+  
