8dffa01049713fa3cb4906fffecc367637f48e3d
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-03 11:33:17 +0300

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -8,13 +8,10 @@
  * @throws {Error} - Throws an error if the position is outside the range.
  */
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+    if (position >= length) {
+      throw new Error('Position outside range');
+    }
+    const buffer = new DataView(new ArrayBuffer(length), 0, length);
+    buffer.setInt8(position, value);
+    return buffer;
   }
-
-  const buffer = new ArrayBuffer(length);
-  const dataView = new DataView(buffer);
-  dataView.setInt8(position, value);
-
-  return dataView;
-}
