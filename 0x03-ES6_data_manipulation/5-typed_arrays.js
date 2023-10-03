c8a396f85a947aa85e19de30f160867aa159ee94
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-03 11:31:11 +0300

    Task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,20 @@
+/**
+ * Creates a new ArrayBuffer with an Int8 value at a specific position.
+ *
+ * @param {number} length - The length of the new ArrayBuffer.
+ * @param {number} position - The position to set the Int8 value.
+ * @param {number} value - The Int8 value to set.
+ * @returns {DataView} - A DataView with the specified Int8 value at the given position.
+ * @throws {Error} - Throws an error if the position is outside the range.
+ */
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
