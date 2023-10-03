54f79bfd12e262594801950b471e91909456d374
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-03 11:35:43 +0300

    Task 6

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Converts an array into a Set.
+ *
+ * @param {Array} array - The array to convert into a Set.
+ * @returns {Set} - A Set containing the elements of the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
