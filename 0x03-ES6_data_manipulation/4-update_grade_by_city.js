d501bf9cd4873ea6e0cb53547beec298b0e2d515
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-03 10:02:51 +0300

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,16 +7,15 @@
  * @returns {Object[]} - An array of students with updated grades.
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students)) return [];
-
-  const defaultGrade = { grade: 'N/A' };
-
-  return students
-    .filter(student => student.location === city)
-    .map(student => ({
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grade: (newGrades.find(grade => grade.studentId === student.id) || defaultGrade).grade,
-    }));
-}
+    const defaultGrade = { grade: 'N/A' };
+  
+    if (students instanceof Array) {
+      return students
+        .filter((student) => student.location === city).map((student) => ({
+          id: student.id,
+          firstName: student.firstName,
+          location: student.location,
+          grade: (newGrades.filter((grade) => grade.studentId === student.id).pop() || defaultGrade).grade,}));
+    }
+    return [];
+  }
