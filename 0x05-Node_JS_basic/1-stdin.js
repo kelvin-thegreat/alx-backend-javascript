00f4f732a2d023e209340bb6a36ce015b761108a
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-20 12:36:18 +0300

    Task 1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+// 1-stdin.js executes through command line:
+
+/* 
+It displays the message Welcome to Holberton School, what is your name?
+The user inputs their name on a new line
+The program display Your name is: INPUT
+When the user ends the program, it displays "This important software is now closing"
+*/
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
