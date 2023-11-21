abfdd1d265e5f784d70aa7eb070725bd5fdb6ddf
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-21 21:15:33 +0300

    Task 7. Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,14 +1,5 @@
-/*
-Small HTTP server using Express:
-Its assigned to the variable app and exported
-HTTP server listens on port 1245
-It returns plain text
-When the URL path is /, it displays Hello Holberton School! in the page body
-When the URL path is /students, it display This is the list of our students. 
-*/
 const express = require('express');
-// Using fs.promises for promise-based file operations
-const fs = require('fs').promises; 
+const fs = require('fs'); 
 
 const app = express();
 const PORT = 1245;
@@ -17,7 +8,6 @@ const DB_FILE = process.argv[2] || '';
 const countStudents = async (dataPath) => {
   try {
     const data = await fs.readFile(dataPath, 'utf-8');
-    // ... (rest of the function remains the same)
   } catch (err) {
     throw new Error('Cannot load the database');
   }
