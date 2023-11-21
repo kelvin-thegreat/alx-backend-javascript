4c6bd486bd2331862405ff6e712531e99ceb0c48
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-21 21:18:00 +0300

    Task 7. Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,42 +1,85 @@
 const express = require('express');
-const fs = require('fs'); 
+const fs = require('fs');
 
 const app = express();
 const PORT = 1245;
-const DB_FILE = process.argv[2] || '';
+const DB_FILE = process.argv[2] ? process.argv[2] : '';
 
-const countStudents = async (dataPath) => {
-  try {
-    const data = await fs.readFile(dataPath, 'utf-8');
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-};
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  !dataPath ? reject(new Error('Cannot load the database')) : null;
 
-const handleErrors = (res, err) => {
-  const errorMessage = err instanceof Error ? err.message : err.toString();
-  const responseText = `Error: ${errorMessage}`;
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', Buffer.from(responseText).length);
-  res.status(500).send(responseText);
-};
+  fs.readFile(dataPath, (err, data) => {
+    err ? reject(new Error('Cannot load the database')) : null;
+
+    const reportParts = [];
+    const fileLines = data.toString('utf-8').trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(
+      0,
+      dbFieldNames.length - 1,
+    );
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(
+        0,
+        studentRecord.length - 1,
+      );
+      const field = studentRecord[studentRecord.length - 1];
+      !Object.keys(studentGroups).includes(field) ? studentGroups[field] = [] : null;
+
+      const studentEntries = studentPropNames.map((propName, idx) => [
+        propName,
+        studentPropValues[idx],
+      ]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce(
+      (pre, cur) => (pre || []).length + cur.length,
+    );
+    reportParts.push(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      reportParts.push([
+        `Number of students in ${field}: ${group.length}.`,
+        'List:',
+        group.map((student) => student.firstname).join(', '),
+      ].join(' '));
+    }
+    resolve(reportParts.join('\n'));
+  });
+});
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (_, res) => {
-  try {
-    const responseParts = ['This is the list of our students'];
-    const report = await countStudents(DB_FILE);
-    responseParts.push(report);
-    const responseText = responseParts.join('\n');
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', Buffer.from(responseText).length);
-    res.status(200).send(responseText);
-  } catch (err) {
-    handleErrors(res, err);
-  }
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
+
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    })
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    });
 });
 
 app.listen(PORT, () => {
