f0b0a3a8d02f4fa18326a194848c04cebc703204
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-21 20:35:05 +0300

    Task 4. Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,21 +1,20 @@
-/* eslint-disable jest/require-hook */
 const http = require('http');
 
-// eslint-disable-next-line linebreak-style
-// Create simple HTTP server
-const app = http.createServer((req, res) => {
-  // Set content type to plain text
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Send "Hello Holberton School!" as the response
-  res.end('Hello Holberton School!\n');
-});
-
-// Set server to listen port 1245
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-// Export app
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
 module.exports = app;
