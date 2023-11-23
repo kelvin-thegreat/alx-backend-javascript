6358cb5ab246f09cea92714de23ac22c23103c3c
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-23 23:27:06 +0300

    Task 6. Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,13 @@
+// 6-payment_token.js
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    } else {
+      // If success is false, do nothing
+      resolve();
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
