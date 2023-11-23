b490bde9957106dbcd5edb91739a0379db40d17c
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-11-23 23:43:55 +0300

    Task 8. Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,39 +1,14 @@
-// api.test.js
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Index page', () => {
-  let server;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    // Start the server before running tests
-    server = app.listen(7865, () => {
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
-  });
-
-  after(() => {
-    // Close the server after running tests
-    server.close();
-  });
-
-  it('should return correct status code', (done) => {
-    request.get('http://localhost:7865/', (error, response) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return correct result', (done) => {
-    request.get('http://localhost:7865/', (error, response, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should handle other cases', (done) => {
-    // Add additional test cases if needed
-    done();
   });
 });
